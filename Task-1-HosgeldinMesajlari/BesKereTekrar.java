edd8f09bfc3e08cd66eaabada651376221a2361c
250541018 <250541018@firat.edu.tr>
2025-10-22 21:08:23 +0300

    Update BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,10 @@
+public class UcMesaj {
+    public static void main(String[] args) {
+
+        System.out.println("java'ya Hoş Geldiniz");
+        System.out.println("java'ya Hoş Geldiniz");
+        System.out.println("java'ya Hoş Geldiniz");
+        System.out.println("java'ya Hoş Geldiniz");
+        System.out.println("java'ya Hoş Geldiniz");
+    }
+}
