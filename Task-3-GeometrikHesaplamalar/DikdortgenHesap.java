67ffb810486a3f0dd318845907aee79daecf5c46
250541018 <250541018@firat.edu.tr>
2025-10-23 12:47:41 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,15 @@
+public class DikdortgenHesaplama {
+    public static void main(String[] args) {
+        double en = 4.5;
+        double boy = 7.9;
+
+        double alan = en * boy;
+        double cevre = 2 * (en + boy);
+
+        System.out.println("Dikdörtgen Alan ve Çevre Hesaplama");
+        System.out.println("---------------------------------");
+        System.out.println("En: " + en + ", Boy: " + boy);
+        System.out.println("Alan: " + alan);
+        System.out.println("Çevre: " + cevre);
+    }
+}
