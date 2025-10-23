976d315629cebd8765f556bebf071a747c0d888d
250541018 <250541018@firat.edu.tr>
2025-10-23 13:03:04 +0300

    Update CarpimTablosu.java

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,9 @@
+public class CarpimTablosu {
+    public static void main(String[] args) {
+        int sayi = 2;
+        System.out.println("2'nin Çarpım Tablosu:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(sayi + " x " + i + " = " + (sayi * i));
+        }
+    }
+}
