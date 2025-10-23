83d02743b315fb0221f719df83570b73227a26f7
250541018 <250541018@firat.edu.tr>
2025-10-23 13:07:43 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,11 @@
+public class ASCIISanat {
+    public static void main(String[] args) {
+        System.out.println(" E D A N U R ");
+        System.out.println(" _____  _____  _____  _   _  _____  _____ ");
+        System.out.println("| ____|| ____||  _  || | | ||  _  || ____|");
+        System.out.println("| |__  | |__  | |_| || |_| || |_| || |__  ");
+        System.out.println("|___ \\ |___ \\ |  _  ||  _  ||  _  ||___ \\ ");
+        System.out.println(" ___) | ___) || | | || | | || | | | ___) |");
+        System.out.println("|____/ |____/ |_| |_||_| |_||_| |_||____/ ");
+    }
+}
